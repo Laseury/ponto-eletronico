27bf4fcad5dc579ed24a3a35641a99ff4c304203
Luiz <luizfernando7254@gmail.com>
2026-03-27 10:06:50 -0300

    Atualização VISUAL de botões

--- backend/routes/funcionarios.routes.js
+++ backend/routes/funcionarios.routes.js
@@ -4,11 +4,15 @@ const router = express.Router();
 const {
   listarFuncionarios,
   buscarFuncionarioPorId,
-  criarFuncionario
+  criarFuncionario,
+  editarFuncionario,
+  alternarAtivo,
 } = require("../controllers/funcionarios.controller");
 
 router.get("/", listarFuncionarios);
 router.get("/:id", buscarFuncionarioPorId);
 router.post("/", criarFuncionario);
+router.put("/:id", editarFuncionario);
+router.patch("/:id/ativo", alternarAtivo);
 
 module.exports = router;
